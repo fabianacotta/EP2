9ba609a66ea2da0522f4f8e0b937ca617f008964
fabianacotta <fabianacffl@al.insper.edu.br>
2025-04-29 11:54:16 -0300

    Update funcoes.py
    Q1

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,10 @@
-
+import random
+def rolar_dados (numero_dados):
+  sorteados = [] 
+  i = 0 
+  while i < numero_dados:
+    valor = random.randint(1,6)
+    sorteados.append(valor)
+    i+=1
+  return sorteados 
+    
